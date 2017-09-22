9786b4ed6bfa76e602774db8e491d8dbcdfd4849
Sebastian Diehl <mail@diehl-s.de>
2017-09-22 16:21:05 +0200

    no message

--- install_rmq.sh
+++ install_rmq.sh
@@ -7,6 +7,10 @@ yum install wget -y
 wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/create_ca_and_cert.sh
 chmod +x create_ca_and_cert.sh
 source ./create_ca_and_cert.sh
+cp server/cert.pem /etc/rabbitmq/ssl/
+cp server/key.pem /etc/rabbitmq/ssl/
+cp ca/cacert.pem /etc/rabbitmq/ssl/
+cat /etc/rabbitmq/ssl/cert.pem /etc/rabbitmq/ssl/key.pem > /etc/rabbitmq/ssl/inter-node.pem
 yum install epel-release -y
 wget https://packages.erlang-solutions.com/erlang-solutions-1.0-1.noarch.rpm
 rpm -Uvh erlang-solutions-1.0-1.noarch.rpm
@@ -28,6 +32,7 @@ rabbitmqctl set_user_tags $RMQ_ADMIN administrator
 rabbitmqctl set_permissions -p / $RMQ_ADMIN ".*" ".*" ".*"
 wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/rabbitmq.config -O /etc/rabbitmq/rabbitmq.config
 wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/rabbitmq-env.conf  -O /etc/rabbitmq/rabbitmq-env.conf
+chown rabbitmq:rabbitmq /etc/rabbitmq/* -R
 systemctl restart rabbitmq-server
 if if [ "$MASTERNODE" == "$HOSTNAME" ]
     then

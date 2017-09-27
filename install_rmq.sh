d9e51007c6e2b2c81ed14a0ec8d3c9c96e503f34
Sebastian Diehl <mail@diehl-s.de>
2017-09-27 14:35:36 +0200

    no message

--- install_rmq.sh
+++ install_rmq.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 export RMQ_ADMIN=admin
+#echo "Please enter the Password for the RabbitMQ admin" 
+#read -sr RMQ_PASSWORD_INPUT
+#export RMQ_PASSWORD=$RMQ_PASSWORD_INPUT
 export RMQ_PASSWORD=password
+#echo "Do you want to deploy a RabbitMQ Cluster node?(yes/no)" 
 export RMQCLUSTER=true
 yum install wget -y
 yum install epel-release -y
@@ -11,27 +15,39 @@ yum install socat -y
 wget https://bintray.com/rabbitmq/rabbitmq-server-rpm/download_file?file_path=rabbitmq-server-3.6.9-1.el6.noarch.rpm -O rmq.npm
 rpm -ihv rmq.npm
 rabbitmq-plugins enable rabbitmq_management
-systemctl stop rabbitmq-server
-mkdir /etc/rabbitmq/ssl
-cp server/cert.pem /etc/rabbitmq/ssl/
-cp server/key.pem /etc/rabbitmq/ssl/
-cp ca/cacert.pem /etc/rabbitmq/ssl/
-cat /etc/rabbitmq/ssl/cert.pem /etc/rabbitmq/ssl/key.pem > /etc/rabbitmq/ssl/inter-node.pem
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
 rabbitmqctl add_user $RMQ_ADMIN $RMQ_PASSWORD
 rabbitmqctl set_user_tags $RMQ_ADMIN administrator
 rabbitmqctl set_permissions -p / $RMQ_ADMIN ".*" ".*" ".*"
-systemctl restart rabbitmq-server
 wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/rabbitmq.config -O /etc/rabbitmq/rabbitmq.config
 wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/rabbitmq-env.conf  -O /etc/rabbitmq/rabbitmq-env.conf
-chown rabbitmq:rabbitmq /etc/rabbitmq/* -R
+
 if [ "$RMQCLUSTER" == "true" ]
 then
+    if [ "$MASTERNODE" == "$HOSTNAME" ]
+    then
+    wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/create_ca_and_cert.sh
+    chmod +x create_ca_and_cert.sh
+    source ./create_ca_and_cert.sh
+    mkdir /etc/rabbitmq/ssl /usr/share/ssl/
+    cp server/cert.pem /usr/share/ssl/
+    cp server/key.pem /usr/share/ssl/
+    cp ca/cacert.pem /usr/share/ssl/
+    cp server/cert.pem /etc/rabbitmq/ssl/
+    cp server/key.pem /etc/rabbitmq/ssl/
+    cp ca/cacert.pem /etc/rabbitmq/ssl/
+    cat /etc/rabbitmq/ssl/cert.pem /etc/rabbitmq/ssl/key.pem > /etc/rabbitmq/ssl/inter-node.pem
+    else
+
+    fi
     wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/join_cluster.sh -O /etc/rabbitmq/join_cluster.sh
     chmod +x /etc/rabbitmq/join_cluster.sh
+    chmod 777 /etc/rabbitmq/join_cluster.sh
     echo "source /etc/rabbitmq/join_cluster.sh" >> /etc/rc.d/rc.local
+    chown rabbitmq:rabbitmq /etc/rabbitmq/* -R
     chmod +x /etc/rc.d/rc.local
     reboot
+    
 else
     wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/create_ca_and_cert.sh
     chmod +x create_ca_and_cert.sh
--- join_cluster.sh
+++ join_cluster.sh
@@ -1,22 +1,16 @@
 export MASTERNODE=rmq-1
 if [ "$MASTERNODE" == "$HOSTNAME" ]
     then
-        wget https://raw.githubusercontent.com/evoila/vcd-rabbitmq-cluster-config/master/create_ca_and_cert.sh
-        chmod +x create_ca_and_cert.sh
-        source ./create_ca_and_cert.sh
-        mkdir /etc/rabbitmq/ssl /usr/share/ssl/
-        cp server/cert.pem /usr/share/ssl/
-        cp server/key.pem /usr/share/ssl/
-        cp ca/cacert.pem /usr/share/ssl/
-        cp server/cert.pem /etc/rabbitmq/ssl/
-        cp server/key.pem /etc/rabbitmq/ssl/
-        cp ca/cacert.pem /etc/rabbitmq/ssl/
+      
+        systemctl 
+
     else
         mkdir /etc/rabbitmq/ssl
         scp centos@$MASTERNODE:/usr/share/ssl/* /etc/rabbitmq/ssl/
+        chown rabbitmq:rabbitmq /etc/rabbitmq/* -R
         rabbitmqctl stop_app
         rabbitmqctl join_cluster rabbit@$MASTERNODE
         rabbitmqctl start_app
 fi
 rabbitmqctl set_policy ha-all “” ‘{“ha-mode”:“all”,“ha-sync-mode”:“automatic”}’
-sed -i  '/join_cluster.sh/d' /etc/rc.d/rc.local
+#sed -i  '/join_cluster.sh/d' /etc/rc.d/rc.local

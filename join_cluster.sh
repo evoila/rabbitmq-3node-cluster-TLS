add2c261403e7ba373fa9ecb5507ccfbbdf76b2e
Sebastian Diehl <mail@diehl-s.de>
2017-09-29 13:55:56 +0200

    no message

--- join_cluster.sh
+++ join_cluster.sh
@@ -1,13 +1,12 @@
 export MASTERNODE=rmq-1
-if [ "$MASTERNODE" == "$HOSTNAME" ]
+if [ "$MASTERNODE" != "$HOSTNAME" ]
     then
-    else
         mkdir /etc/rabbitmq/ssl
-        scp centos@$MASTERNODE:/usr/share/ssl/* /etc/rabbitmq/ssl/
+        scp $MASTERNODE:/usr/share/ssl/* /etc/rabbitmq/ssl/
         chown rabbitmq:rabbitmq /etc/rabbitmq/* -R
         rabbitmqctl stop_app
         rabbitmqctl join_cluster rabbit@$MASTERNODE
         rabbitmqctl start_app
 fi
 rabbitmqctl set_policy ha-all “” ‘{“ha-mode”:“all”,“ha-sync-mode”:“automatic”}’
-#sed -i  '/join_cluster.sh/d' /etc/rc.d/rc.local
+sed -i  '/join_cluster.sh/d' /etc/rc.d/rc.local
